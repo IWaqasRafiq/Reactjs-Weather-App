abee01e146020857afb61531dcd5bd2be570b4db
waqas rafiq <wm66179@gmail.com>
2023-09-02 00:34:13 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import './App.css';
+import Home from "./components/Home/home";
+
+const App = () => <Home />
+
+export default App;
---
+++ src/components/Home/home.jsx
@@ -0,0 +1,13 @@
+import { useState, useRef } from "react";
+import React from "react";
+
+const Home = () => {
+    const [weatherdata, setWeatherdata] = useState([]);
+    const cityNameRef = useRef(null);
+
+    const submithandlet = async () => {
+        e.preventDefault();
+
+        
+    }
+}
